998d8401623cdf9d7ea0023ec3b4c89855c13605
AGO523 <unpouletfrais0523@gmail.com>
2022-05-25 11:55:27 +0900

    actions

--- test.sh
+++ test.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 
-echo "パーソナルアクセストークンを入力してください"
-read PERSONAL_ACCESS_TOKEN
+# echo "パーソナルアクセストークンを入力してください"
+# read PERSONAL_ACCESS_TOKEN
 
 echo "マージするプルリクエストのナンバーを入力してください"
 read PULL_NUMBER
@@ -9,13 +9,9 @@ read PULL_NUMBER
 echo "予約する日時を入力してください。例:202205241900 -> 2022年5月24日19時00分"
 read RELESE_DATETIME
 
-# command= echo "curl -X PUT \
-# -H "Accept: application/vnd.github.v3+json" \
-# -H "Authorization: token $PERSONAL_ACCESS_TOKEN" \
-# https://api.github.com/repos/AGO523/merge_sample/pulls/$PULL_NUMBER/merge" | at -t "$RELESE_DATETIME"
 command= echo "curl \
 -X PUT \
 -H "Accept: application/vnd.github.v3+json" \
--H "Authorization: token ${PERSONAL_ACCESS_TOKEN}" \
-https://api.github.com/repos/AGO523/merge_sample/pulls/${PULL_NUMBER}/merge" | at -t "$RELESE_DATETIME"
+-H "Authorization: token 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" \
+https://api.github.com/repos/AGO523/merge_sample/pulls/${PULL_NUMBER}/merge" | at -t "${RELESE_DATETIME}"
 eval $command
